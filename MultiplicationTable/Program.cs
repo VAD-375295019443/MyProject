0135c9182de5c66c36d440c7ccab5a26a2570239
VAD-375295019443 <105417922+VAD-375295019443@users.noreply.github.com>
2022-05-30 22:17:08 +0300

    Изменения

--- MultiplicationTable/Program.cs
+++ MultiplicationTable/Program.cs
@@ -8,61 +8,13 @@ namespace MultiplicationTable
         {
             string? strNumber;
             byte bytNumberMin = 1;
-            ulong uloNumberMax = ulong.MaxValue / 10;
+            ulong ulonNumberMax = ulong.MaxValue / 10;
             ulong ulonNumber;
             bool booError;
-            
+
             while (1 == 1)
             {
-                Console.WriteLine($"Введите целое число в диапазоне от {bytNumberMin} по {uloNumberMax}. Для выхода наберите: Exit."); ;
-                strNumber = Console.ReadLine();
-
-                if(strNumber == "Exit" || strNumber == "exit")
-                {
-                    Console.WriteLine("Goodbye.");
-                    break;
-                }
-
-                booError = ulong.TryParse(strNumber, out ulonNumber);
-                
-                if(booError==false)
-                {
-                    Console.WriteLine("Не корректный ввод данных! Повторите попытку.");
-                    Console.WriteLine("");
-                    continue;
-                }
-                else
-                {
-                    if(ulonNumber<bytNumberMin || ulonNumber > uloNumberMax)
-                    {
-                        Console.WriteLine("Не корректный ввод данных! Повторите попытку.");
-                        Console.WriteLine("");
-                        continue;
-                    }
-                    else
-                    {
-                        F_voiMultiplicationTable(ulonNumber);
-                        Console.WriteLine("");
-                    }
-                }
-            }
-
-            static void F_voiMultiplicationTable(ulong f_ulonNumber)
-            {
-                ulong ulonResult;
-                for(uint int1=1; int1<=10; int1++)
-                {
-                    ulonResult = f_ulonNumber * int1;
-                    Console.WriteLine($"{f_ulonNumber} x {int1} = {ulonResult}");
-                }
-            }
-            
-
-            //Или так:
-            /*
-            while (1 == 1)
-            {
-                Console.WriteLine($"Введите целое число в диапазоне от {bytNumberMin} по {uloNumberMax}. Для выхода наберите: Exit."); ;
+                Console.WriteLine($"Введите целое число в диапазоне от {bytNumberMin} по {ulonNumberMax}. Для выхода наберите: Exit."); ;
                 strNumber = Console.ReadLine();
 
                 if (strNumber == "Exit" || strNumber == "exit")
@@ -70,40 +22,30 @@ namespace MultiplicationTable
                     Console.WriteLine("Goodbye.");
                     break;
                 }
-                //Первый шлюз пройден. (Мы не требуем выхода из консоли).
 
                 booError = ulong.TryParse(strNumber, out ulonNumber);
-                if (booError == false)
+
+                if (booError == false || ulonNumber < bytNumberMin || ulonNumber > ulonNumberMax)
                 {
                     Console.WriteLine("Не корректный ввод данных! Повторите попытку.");
                     Console.WriteLine("");
                     continue;
                 }
-                //Второй шлюз пройден. (Введенные данные успешно конвертированы).
-
-                if (ulonNumber < bytNumberMin || ulonNumber > uloNumberMax)
-                {
-                    Console.WriteLine("Не корректный ввод данных! Повторите попытку.");
-                    Console.WriteLine("");
-                    continue;
-                }
-                //Третий шлюз пройден. (Конвертированные данные находятся в нужном диапазоне).
-                //Т.е. ТОТАЛЬНЫЙ УСПЕХ при вводе данных!
-
+                
                 F_voiMultiplicationTable(ulonNumber);
                 Console.WriteLine("");
             }
-
-            static void F_voiMultiplicationTable(ulong f_ulonNumber)
+        }
+        
+        
+        static void F_voiMultiplicationTable(ulong f_ulonNumber)
+        {
+            ulong ulonResult;
+            for(uint int1=1; int1<=10; int1++)
             {
-                ulong ulonResult;
-                for (uint int1 = 1; int1 <= 10; int1++)
-                {
-                    ulonResult = f_ulonNumber * int1;
-                    Console.WriteLine($"{f_ulonNumber} x {int1} = {ulonResult}");
-                }
+                ulonResult = f_ulonNumber * int1;
+                Console.WriteLine($"{f_ulonNumber} x {int1} = {ulonResult}");
             }
-            */
         }
     }
 }
--- NameOfTheDay/Program.cs
+++ NameOfTheDay/Program.cs
@@ -25,32 +25,33 @@ namespace NameOfTheDay
                     Console.WriteLine("");
                 }
             }
+        }
+        
 
-            static string F_strNameOfTheDay(string? f_strDayNumber)
+        static string F_strNameOfTheDay(string? f_strDayNumber)
+        {
+            switch (f_strDayNumber)
             {
-                switch (f_strDayNumber)
-                {
-                    case "1":
-                        return "Monday.";
-                    case "2":
-                        return "Tuesday.";
-                    case "3":
-                        return "Wednesday.";
-                    case "4":
-                        return "Thursday.";
-                    case "5":
-                        return "Friday.";
-                    case "6":
-                        return "Saturday.";
-                    case "7":
-                        return "Sunday.";
-                    case "Exit":
-                        return "Exit";
-                    case "exit":
-                        return "Exit";
-                }
-                return "Не корректный ввод данных! Повторите попытку.";
+                case "1":
+                    return "Monday.";
+                case "2":
+                    return "Tuesday.";
+                case "3":
+                    return "Wednesday.";
+                case "4":
+                    return "Thursday.";
+                case "5":
+                    return "Friday.";
+                case "6":
+                    return "Saturday.";
+                case "7":
+                    return "Sunday.";
+                case "Exit":
+                    return "Exit";
+                case "exit":
+                    return "Exit";
             }
+            return "Не корректный ввод данных! Повторите попытку.";
         }
     }
 }
