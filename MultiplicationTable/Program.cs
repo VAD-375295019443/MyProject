76a55eab05cee11152af25c3536f7e2facb6b24b
VAD-375295019443 <105417922+VAD-375295019443@users.noreply.github.com>
2022-05-30 15:15:55 +0300

    Add project files.

---
+++ MultiplicationTable/Program.cs
@@ -0,0 +1,109 @@
+﻿using System;
+
+namespace MultiplicationTable
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string? strNumber;
+            byte bytNumberMin = 1;
+            ulong uloNumberMax = ulong.MaxValue / 10;
+            ulong ulonNumber;
+            bool booError;
+            
+            while (1 == 1)
+            {
+                Console.WriteLine($"Введите целое число в диапазоне от {bytNumberMin} по {uloNumberMax}. Для выхода наберите: Exit."); ;
+                strNumber = Console.ReadLine();
+
+                if(strNumber == "Exit" || strNumber == "exit")
+                {
+                    Console.WriteLine("Goodbye.");
+                    break;
+                }
+
+                booError = ulong.TryParse(strNumber, out ulonNumber);
+                
+                if(booError==false)
+                {
+                    Console.WriteLine("Не корректный ввод данных! Повторите попытку.");
+                    Console.WriteLine("");
+                    continue;
+                }
+                else
+                {
+                    if(ulonNumber<bytNumberMin || ulonNumber > uloNumberMax)
+                    {
+                        Console.WriteLine("Не корректный ввод данных! Повторите попытку.");
+                        Console.WriteLine("");
+                        continue;
+                    }
+                    else
+                    {
+                        F_voiMultiplicationTable(ulonNumber);
+                        Console.WriteLine("");
+                    }
+                }
+            }
+
+            static void F_voiMultiplicationTable(ulong f_ulonNumber)
+            {
+                ulong ulonResult;
+                for(uint int1=1; int1<=10; int1++)
+                {
+                    ulonResult = f_ulonNumber * int1;
+                    Console.WriteLine($"{f_ulonNumber} x {int1} = {ulonResult}");
+                }
+            }
+            
+
+            //Или так:
+            /*
+            while (1 == 1)
+            {
+                Console.WriteLine($"Введите целое число в диапазоне от {bytNumberMin} по {uloNumberMax}. Для выхода наберите: Exit."); ;
+                strNumber = Console.ReadLine();
+
+                if (strNumber == "Exit" || strNumber == "exit")
+                {
+                    Console.WriteLine("Goodbye.");
+                    break;
+                }
+                //Первый шлюз пройден. (Мы не требуем выхода из консоли).
+
+                booError = ulong.TryParse(strNumber, out ulonNumber);
+                if (booError == false)
+                {
+                    Console.WriteLine("Не корректный ввод данных! Повторите попытку.");
+                    Console.WriteLine("");
+                    continue;
+                }
+                //Второй шлюз пройден. (Введенные данные успешно конвертированы).
+
+                if (ulonNumber < bytNumberMin || ulonNumber > uloNumberMax)
+                {
+                    Console.WriteLine("Не корректный ввод данных! Повторите попытку.");
+                    Console.WriteLine("");
+                    continue;
+                }
+                //Третий шлюз пройден. (Конвертированные данные находятся в нужном диапазоне).
+                //Т.е. ТОТАЛЬНЫЙ УСПЕХ при вводе данных!
+
+                F_voiMultiplicationTable(ulonNumber);
+                Console.WriteLine("");
+            }
+
+            static void F_voiMultiplicationTable(ulong f_ulonNumber)
+            {
+                ulong ulonResult;
+                for (uint int1 = 1; int1 <= 10; int1++)
+                {
+                    ulonResult = f_ulonNumber * int1;
+                    Console.WriteLine($"{f_ulonNumber} x {int1} = {ulonResult}");
+                }
+            }
+            */
+        }
+    }
+}
---
+++ NameOfTheDay/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace NameOfTheDay
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string? strDayNumber;
+
+            while (1 == 1) //Исключительно для удобства.
+            {
+                Console.WriteLine("Введите номер дня недели (1 - понедельник, 2 - вторник и т.д.). Для выхода наберите: Exit.");
+                strDayNumber = Console.ReadLine();
+                strDayNumber = F_strNameOfTheDay(strDayNumber); //Использую туже переменную для уменьшения их количества (память экономлю:)).
+
+                if (strDayNumber == "Exit")
+                {
+                    Console.WriteLine("Goodbye.");
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine(strDayNumber);
+                    Console.WriteLine("");
+                }
+            }
+
+            static string F_strNameOfTheDay(string? f_strDayNumber)
+            {
+                switch (f_strDayNumber)
+                {
+                    case "1":
+                        return "Monday.";
+                    case "2":
+                        return "Tuesday.";
+                    case "3":
+                        return "Wednesday.";
+                    case "4":
+                        return "Thursday.";
+                    case "5":
+                        return "Friday.";
+                    case "6":
+                        return "Saturday.";
+                    case "7":
+                        return "Sunday.";
+                    case "Exit":
+                        return "Exit";
+                    case "exit":
+                        return "Exit";
+                }
+                return "Не корректный ввод данных! Повторите попытку.";
+            }
+        }
+    }
+}
